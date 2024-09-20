2326902c7853154c00cc059a415aa464584fa725
Dominique Degottex <domidex01@gmail.com>
2024-09-20 12:12:29 +0700

    to do changes

--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -74,10 +74,7 @@ const ToDo = () => {
             />
           </div>
 
-          <div
-            className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12
-          '
-          >
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 '>
             {todos.map((todo) => (
               <TodoCard
                 key={todo.id}
